5290aed6778ad8a62dc07f77f670aec344c82a10
Lucas de Mello Santos <lucas.sports@hotmail.com>
2021-10-27 12:55:12 -0300

    padronização response api cadastro administrador

--- back-end/WorkflowProfessional/cadastro-admin/controllers/cadastroAdmin-controller.js
+++ back-end/WorkflowProfessional/cadastro-admin/controllers/cadastroAdmin-controller.js
@@ -1,60 +1,68 @@
 const db = require('../mysql').db;
 
 exports.ObterPorEmail = (req, res) => {
-  const { email } = req.params;
-  let mysql = 'call cadastro_admin_obter_email_sps(?)';
-
   db.getConnection((error, conn) => {
-      conn.query(mysql, [email], (error, result, field) => {
-          conn.release();
-          if (error) {
-            return res.status(500).send({
-                error: error,
-                data: null
-            });
-          } 
-          res.status(200).send({
-              mensagem: '',
-              data: result
-          });            
-        });
+    if (error) { return res.status(500).send({ error: error }) }
+    const { email } = req.params;
+    let mysql = 'call cadastro_admin_obter_email_sps(?)';
+    conn.query(mysql, [email], (error, result, field) => {
+      conn.release();
+      if (error) { return res.status(500).send({ error: error, data: null }) } 
+      const response = {
+        data: {
+          totalRegistros: result[0].length,
+          dados: result[0].map(cadastro => {
+            return {
+              id: cadastro.id,
+              nome: cadastro.nome,
+              email: cadastro.email,
+              empresa: cadastro.empresa,
+              cargo: cadastro.cargo
+            }            
+          }) 
+        }
+      } 
+      return res.status(200).send(response);            
+    });
   })
 }
 
 exports.Cadastrar = (req, res) => {
-  const { id } = req.body;
-  const { nome } = req.body;
-  const { email } = req.body;
-  const { empresa } = req.body;
-  const { cargo } = req.body;
-
-  let mysql = "call cadastro_admin_incluir_spi(?,?,?,?,?)";
-  db.query(mysql, [id, nome, email, empresa, cargo], (err, result) => {
-      if (err) {
-          console.log(err);
-      } else {
-          res.status(201).send({
-            data: result
-          });
-      }
-  });
+  db.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    const { id } = req.body;
+    const { nome } = req.body;
+    const { email } = req.body;
+    const { empresa } = req.body;
+    const { cargo } = req.body;
+    let mysql = "call cadastro_admin_incluir_spi(?,?,?,?,?)";
+    conn.query(mysql, [id, nome, email, empresa, cargo], (error, result, field) => {
+      conn.release();
+      if (error) { return res.status(500).send({ error: error, data: null }) } 
+      const response = {
+        data: { dados: 'Dados Cadastrados com sucesso!' }
+      } 
+      return res.status(201).send(response);            
+    });
+  })  
 }
 
 exports.Atualizar = (req, res) => {
-  const { id } = req.params;    
-  const { nome } = req.body;
-  const { email } = req.body;
-  const { empresa } = req.body;
-  const { cargo } = req.body;
-
-  let mysql = "call cadastro_admin_alterar_spi(?,?,?,?,?)";
-  db.query(mysql, [id, nome, email, empresa, cargo], (err, result) => {
-    if (err) {
-        console.log(err);
-      } else {
-        res.status(201).send({
-          data: result
-        });
-      }
-  });
+  db.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    const { id } = req.params;    
+    const { nome } = req.body;
+    const { email } = req.body;
+    const { empresa } = req.body;
+    const { cargo } = req.body;
+    let mysql = "call cadastro_admin_alterar_spi(?,?,?,?,?)";
+    conn.query(mysql, [id, nome, email, empresa, cargo], (error, result, field) => {
+      conn.release();
+      if (error) { return res.status(500).send({ error: error, data: null }) } 
+      const response = {
+        data: { dados: 'Dados Atualizados com sucesso!' }
+      } 
+      return res.status(201).send(response);            
+    });
+  })
 }
